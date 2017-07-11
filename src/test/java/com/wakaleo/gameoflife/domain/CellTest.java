dfbbb8763bdb0cf840d66659d5069107a03c02e4
forview <huweimails@163.com>
2017-07-11 15:11:55 +0800

    commit

---
+++ src/test/java/com/wakaleo/gameoflife/domain/CellTest.java
@@ -0,0 +1,27 @@
+package com.wakaleo.gameoflife.domain;
+
+import org.junit.Test;
+
+/**
+ * author:huwei
+ * date:2017/7/11
+ * time:15:11
+ * desc:
+ */
+public class CellTest {
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testFromSymbol() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSymbol() throws Exception {
+
+    }
+}
