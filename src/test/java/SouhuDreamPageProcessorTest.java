bea020143aab90ce963d79cbc185e6e287415330
forview <huweimails@163.com>
2017-07-11 12:20:32 +0800

    commit

---
+++ src/test/java/SouhuDreamPageProcessorTest.java
@@ -0,0 +1,70 @@
+package model.souhu;
+
+/**
+ * author:huwei
+  * date:2017/7/11
+   * time:12:03
+    * desc:
+     */
+     public class SouhuDreamPageProcessorTest {
+
+         @org.junit.Test
+	     public void testSetTitle() throws Exception {
+
+	         }
+
+		     @org.junit.Test
+		         public void testSetCategory() throws Exception {
+
+			     }
+
+			         @org.junit.Test
+				     public void testSetUrlAndId() throws Exception {
+
+				         }
+
+					     @org.junit.Test
+					         public void testSetDate() throws Exception {
+
+						     }
+
+						         @org.junit.Test
+							     public void testSetPicAndContent() throws Exception {
+
+							         }
+
+								     @org.junit.Test
+								         public void testSetSourceFrom() throws Exception {
+
+									     }
+
+									         @org.junit.Test
+										     public void testAddExtraInfoWhenHandleListPage() throws Exception {
+
+										         }
+
+											     @org.junit.Test
+											         public void testHandleListPage() throws Exception {
+
+												     }
+
+												         @org.junit.Test
+													     public void testProcess() throws Exception {
+
+													         }
+
+														     @org.junit.Test
+														         public void testGetSite() throws Exception {
+
+															     }
+
+															         @org.junit.Test
+																     public void testAfterProcess() throws Exception {
+
+																         }
+
+																	     @org.junit.Test
+																	         public void testMain() throws Exception {
+
+																		     }
+																		     }
